8cc497f1ef7d7e9a83aa2ec6eeb2dc3520b6f5d0
User1391 <pye3yh@virginia.edu>
2025-05-26 18:42:15 -0400

    deletions

--- python/main.py
+++
@@ -1,8 +0,0 @@
-import camelstructures as cs
-
-board_state = [[cs.Camel.YELLOW, cs.Camel.ORANGE, cs.Camel.WHITE], [cs.Camel.BLUE, cs.Camel.GREEN]]
-token_state = [None, None, cs.Token.MINUS]
-gs = cs.GameState(board_state, token_state)
-
-
-gs.choose_move()
