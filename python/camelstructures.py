e6ddcd84ced24a75a4b52d69a52aa74f719030ec
User1391 <pye3yh@virginia.edu>
2025-05-26 14:07:28 -0400

    got movement working

--- python/camelstructures.py
+++ python/camelstructures.py
@@ -27,7 +27,7 @@ class GameState:
         if token_board != None: self.token_board = token_board
         if free_dice != None: self.free_dice = free_dice
         if free_cards != None: self.free_cards = free_cards 
-
+    
     def eval_game_state(self):
         """Return an ordered list of the camels, from first to fifth for the round."""
         output_list = []
@@ -53,7 +53,39 @@ class GameState:
 
         return output_map
 
-
-    
-
+    def move_camel(self, camel, num_spaces):
+        """Move the camel forward the number of desired spaces, while accounting for plus or minus tokens"""
+        for space_idx in range(len(self.board)):
+            if len(self.board[space_idx]) != 0:
+                for camel_idx in range(len(self.board[space_idx])):
+                    if self.board[space_idx][camel_idx] == camel:
+                        # get sublist of camels from camel we want to move and upwards
+                        camel_stack = self.board[space_idx][camel_idx:]
+                        # now remove that stack 
+                        self.board[space_idx] = self.board[space_idx][:camel_idx]
+                        break
+            # now, we should check forward to see if a token exists at the spot we want to go
+            new_space_idx = space_idx + num_spaces
+            if new_space_idx >= len(self.token_board):
+                # no token, just create or extend as normal
+                # check if the space we're going to even exists 
+                if new_space_idx >= len(self.board):
+                    self.board.insert(new_space_idx, camel_stack)
+                else:
+                    self.board[new_space_idx].extend(camel_stack)
+            else:
+                # make sure it's not just a blank space 
+                if self.token_board[new_space_idx] == None:
+                    # we know it's blank because camels can never be on the same space as tokens
+                    self.board.insert(new_space_idx, camel_stack)
+                elif self.token_board[new_space_idx] == Token.PLUS:
+                    # do same thing as above but for the next space 
+                    if new_space_idx + 1 >= len(self.board):
+                        self.board[new_space_idx + 1] = camel_stack
+                    else:
+                        self.board[new_space_idx + 1].extend(camel_stack)
+                else:
+                    # we know it's a minus space 
+                    self.board[new_space_idx - 1] = camel_stack + self.board[new_space_idx - 1]
+            break
 
--- python/main.py
+++ python/main.py
@@ -1,8 +1,11 @@
 import camelstructures as cs
 
-board_state = [[],[cs.Camel.YELLOW, cs.Camel.ORANGE, cs.Camel.WHITE, cs.Camel.BLUE, cs.Camel.GREEN]]
+board_state = [[cs.Camel.YELLOW, cs.Camel.ORANGE], [cs.Camel.WHITE, cs.Camel.BLUE, cs.Camel.GREEN]]
 gs = cs.GameState(board_state)
 
 print(gs.eval_game_state())
-
+print("")
 print(gs.calculate_outcome())
+print("")
+gs.move_camel(cs.Camel.YELLOW, 2)
+print(gs.eval_game_state())
